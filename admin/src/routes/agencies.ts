e1190689e2cac712310b46c5dc4847bf5017e20d
senntou <wataru_00117@icloud.com>
2024-09-29 15:59:20 +0900

    事務所ページ作成

--- admin/src/routes/agencies.ts
+++ admin/src/routes/agencies.ts
@@ -4,12 +4,12 @@ import express from 'express';
 
 const router = express.Router();
 
-const query = 'SELECT * FROM agency';
+const getQuery = 'SELECT * FROM agency';
 
 router.get('/', async function (_req: Request, res: Response, next: NextFunction) {
   const client = await getClient();
   try {
-    const result = await client.query(query);
+    const result = await client.query(getQuery);
     const agencies = result.rows;
     res.render('agencies', { title: '事務所一覧', agencies });
   } catch (err) {
@@ -17,12 +17,36 @@ router.get('/', async function (_req: Request, res: Response, next: NextFunction
   }
 });
 
+const postQuery = 'INSERT INTO agency(name) VALUES($1)';
+
 router.post('/', async function (req: Request, res: Response, next: NextFunction) {
   const client = await getClient();
   try {
     const { name } = req.body;
-    await client.query('INSERT INTO agency(name) VALUES($1)', [name]);
+    await client.query(postQuery, [name]);
     res.redirect('/agencies');
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id', async function (req: Request, res: Response, next: NextFunction) {
+  const query = `
+  SELECT
+    actor.id,
+    actor.name
+  FROM
+    actor
+    JOIN agency ON actor.agency_id = agency.id
+  WHERE
+    agency.id = $1
+  `;
+
+  const client = await getClient();
+  try {
+    const result = await client.query(query, [req.params.id]);
+    const actors = result.rows;
+    res.render('agencyDetail', { title: '事務所詳細', actors });
   } catch (err) {
     next(err);
   }
