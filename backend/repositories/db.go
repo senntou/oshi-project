ac0c9dcf7ed742f3e37d2e91e919b84127069256
senntou <wataru_00117@icloud.com>
2024-10-13 12:14:02 +0900

    backend用deployのDockerfile作成

--- backend/repositories/db.go
+++ backend/repositories/db.go
@@ -3,27 +3,61 @@ package repositories
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 
 	_ "github.com/lib/pq"
 )
 
+// connectUnixSocket initializes a Unix socket connection pool for
+// a Cloud SQL instance of Postgres.
 func GetDB() (*sql.DB, error) {
-
-	user := os.Getenv("POSTGRES_USER")
-	dbname := os.Getenv("POSTGRES_DB")
-	password := os.Getenv("POSTGRES_PASSWORD")
-
-	dbDriver := "postgres"
-	dsn := fmt.Sprintf(
-		"host=localhost port=5432 user=%s password=%s dbname=%s sslmode=disable",
-		user, password, dbname,
+	mustGetenv := func(k string) string {
+		v := os.Getenv(k)
+		if v == "" {
+			log.Fatalf("Fatal Error in connect_unix.go: %s environment variable not set.\n", k)
+		}
+		return v
+	}
+	// Note: Saving credentials in environment variables is convenient, but not
+	// secure - consider a more secure solution such as
+	// Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+	// keep secrets safe.
+	var (
+		dbUser         = mustGetenv("DB_USER")              // e.g. 'my-db-user'
+		dbPwd          = mustGetenv("DB_PASS")              // e.g. 'my-db-password'
+		unixSocketPath = mustGetenv("INSTANCE_UNIX_SOCKET") // e.g. '/cloudsql/project:region:instance'
+		dbName         = mustGetenv("DB_NAME")              // e.g. 'my-database'
 	)
 
-	db, err := sql.Open(dbDriver, dsn)
+	dbURI := fmt.Sprintf("user=%s password=%s database=%s host=%s",
+		dbUser, dbPwd, dbName, unixSocketPath)
 
+	// dbPool is the pool of database connections.
+	dbPool, err := sql.Open("postgres", dbURI)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("sql.Open: %w", err)
 	}
-	return db, err
+
+	return dbPool, nil
 }
+
+// func GetDB() (*sql.DB, error) {
+
+// 	user := os.Getenv("POSTGRES_USER")
+// 	dbname := os.Getenv("POSTGRES_DB")
+// 	password := os.Getenv("POSTGRES_PASSWORD")
+
+// 	dbDriver := "postgres"
+// 	dsn := fmt.Sprintf(
+// 		"host=localhost port=5432 user=%s password=%s dbname=%s sslmode=disable",
+// 		user, password, dbname,
+// 	)
+
+// 	db, err := sql.Open(dbDriver, dsn)
+
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return db, err
+// }
